b8190bd16bc8c94ee2d03319503e48187efd6d1f
Juan Pablo <juanpablodelorenzo@gmail.com>
2020-01-06 15:46:11 -0300

    Add student name to bills

--- lib/afip_bill/user.rb
+++ lib/afip_bill/user.rb
@@ -1,12 +1,13 @@
 module AfipBill
   class User
-    attr_accessor :company_name, :owner_name, :address, :tax_category
+    attr_accessor :company_name, :owner_name, :address, :tax_category, :student_name
 
-    def initialize(company_name, owner_name, address, tax_category)
+    def initialize(company_name, owner_name, address, tax_category, student_name)
       @company_name = company_name
       @owner_name = owner_name
       @address = address
       @tax_category = tax_category
+      @student_name = student_name
     end
   end
 end
